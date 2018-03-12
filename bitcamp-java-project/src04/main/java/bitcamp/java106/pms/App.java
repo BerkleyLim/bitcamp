ed227e0192ca1dbad36f47b18b741db793bed0e1
berkleylim <berkleylim16@gmail.com>
2018-03-12 11:15:57 +0900

    test

--- bitcamp-java-project/src04/main/java/bitcamp/java106/pms/App.java
+++ bitcamp-java-project/src04/main/java/bitcamp/java106/pms/App.java
@@ -1779,6 +1779,7 @@ public class App {
     // 1단계: 입력 값에서 명령어와 검색어를 구분한다.
     // 2단계: 코드가 바뀌면 그에 따라 적절히 변수명도 바뀔 필요가 있다.
     // 3단계: 입력받을 값을 별도의 변수로 저장
+    // 4단계: 팀명이 없으면 안내 문구를 출력한다.
 package bitcamp.java106.pms;
 
 import bitcamp.java106.pms.domain.Team;
@@ -1869,11 +1870,11 @@ public class App {
 
                 if(option == null) {
                     System.out.println("팀명을 입력하시기 바랍니다.");
+                    System.out.println();
                     continue;
                 }
 
                 System.out.println();
-
             }
 
             System.out.println(menu);
@@ -1882,7 +1883,248 @@ public class App {
     }
 }
 
-*/
+// ver0.8_5 - team/view 명령을 구현
+    // 1단계: 입력 값에서 명령어와 검색어를 구분한다.
+    // 2단계: 코드가 바뀌면 그에 따라 적절히 변수명도 바뀔 필요가 있다.
+    // 3단계: 입력받을 값을 별도의 변수로 저장
+    // 4단계: 팀명이 없으면 안내 문구를 출력한다.
+    // 5단계: 팀명으로 배열을 뒤져 팀 정보를 찾는다.
+    // 6단계: 팀명으로 검색할 때 대소문자로 구분하지 말라.
+package bitcamp.java106.pms;
+
+import bitcamp.java106.pms.domain.Team;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner keyScan = new Scanner(System.in);
+
+        // 레퍼런스 생성(클래스 선언)
+        Team[] teams = new Team[1000];
+        int teamIndex = 0;
+
+        while(true) {
+            Sytem.out.print("명령> ");
+
+            // 대문자를 저절로 소문자로 가공한다. , 
+            // split(" "); -> 입력 받은 문자열은 공백의 기준으로 짤려라
+            // ex) 입력을 "aaa bbb ccc" 라 한다.면
+            // arr[0] = aaa, arr[1] = bbb, arr[2] = ccc
+            // 값으로 저장한다. 
+            // 사용 이유는 : 명령과 검색어로 구분하기 위해서.
+            // 일단 다음올 진행 전 기존에 기능이 잘 되는지 확인!!
+
+            String[] arr = keyScan.nextLine().toLowerCase().split(" ");
+            String menu = arr[0];
+            String option = null;   // 주소 없음
+
+            if(arr.length == 2) {
+                option = arr[1];
+            }
+
+
+            if(menu.equals("quit")) {
+                System.out.println("안녕히 가세요!");
+                break;
+            } else if (menu.equal("help")) {
+                System.out.println("[도움말]");
+                System.out.println("팀 등록 명령 : team/add");
+                System.out.println("팀 조회 명령 : team/list");
+                System.out.println("팀 상세조회 명령 : team/view 팀명");
+                System.out.println("회원 등록 명령 : member/add");
+                System.out.println("회원 조회 명령 : member/list");
+                System.out.println("회원 상세조회 명령 : member/view 아이디");
+                System.out.println("종료 : quit");
+                System.out.println();
+                continue;
+            } else if (menu.equals("team/add")) {
+                System.out.println("[팀 정보 입력]");
+                
+                // 먼저 팀 클래스를 만들고 생성해라!!
+                Team team = new Team();
+                
+
+                System.out.print("팀명? ");
+                team.name = keyScan.nextLine();
+
+                System.out.print("설명? ");
+                team.description = keyScan.nextLine();
+
+                System.out.print("최대인원? ");
+                team.name = keyScan.nextInt();
+                keyScan.nextLine();
+
+                System.out.print("시작일? ");
+                team.startDate = keyScan.nextLine();
+                
+                System.out.print("팀명? ");
+                team.endDate = keyScan.nextLine();
+
+                // 팀 정보가 담겨있는 객체의 주소를 배열에 보관한다.
+                teams[teamIndex++] = team;
+
+                System.out.println();
+                
+                continue;
+            } else if (menu.equals("team/list")) {
+                System.out.println("[팀 목록]");
+                for(int i = 0; i < teamIndex; i++) {
+                    System.out.printf("%s, %d, %s, %s\n", 
+                        teams[i].name,  teams[i].name, 
+                        teams[i].startDate, teams[i].endDate);                   
+                }
+                System.out.println();
+                countinue;
+            } else if (menu.equals("team/view")) {
+                System.out.println("[팀 정보 조회]");
+
+                if(option == null) {
+                    System.out.println("팀명을 입력하시기 바랍니다.");
+                    System.out.println();
+                    continue;
+                }
+
+                for (int i = 0; i < teamIndex; i++) {
+                    if (option.equals(teams[i].name)) {
+                        System.out.printf("팀명 : %s\n", teams[i].name);
+                        System.out.printf("설명 : %s\n", teams[i].description);
+                        System.out.printf("최대인원 : %d\n", teams[i].maxQty);
+                        System.out.printf("기간 : %s ~ %s\n",
+                            teams[i].startDate, teams[i].endDate);
+                        break;
+                    }
+                }
+
+                System.out.println();
+            }
+
+            System.out.println(menu);
+        }
+        
+    }
+}
+
+
+// ver0.8_6 - team/view 명령을 구현
+    // 1단계: 입력 값에서 명령어와 검색어를 구분한다.
+    // 2단계: 코드가 바뀌면 그에 따라 적절히 변수명도 바뀔 필요가 있다.
+    // 3단계: 입력받을 값을 별도의 변수로 저장
+    // 4단계: 팀명이 없으면 안내 문구를 출력한다.
+    // 5단계: 팀명으로 배열을 뒤져 팀 정보를 찾는다.
+    // 6단계: 팀명으로 검색할 때 대소문자로 구분하지 말라.
+package bitcamp.java106.pms;
+
+import bitcamp.java106.pms.domain.Team;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner keyScan = new Scanner(System.in);
+
+        // 레퍼런스 생성(클래스 선언)
+        Team[] teams = new Team[1000];
+        int teamIndex = 0;
+
+        while(true) {
+            Sytem.out.print("명령> ");
+
+            // 대문자를 저절로 소문자로 가공한다. , 
+            // split(" "); -> 입력 받은 문자열은 공백의 기준으로 짤려라
+            // ex) 입력을 "aaa bbb ccc" 라 한다.면
+            // arr[0] = aaa, arr[1] = bbb, arr[2] = ccc
+            // 값으로 저장한다. 
+            // 사용 이유는 : 명령과 검색어로 구분하기 위해서.
+            // 일단 다음올 진행 전 기존에 기능이 잘 되는지 확인!!
+
+            String[] arr = keyScan.nextLine().toLowerCase().split(" ");
+            String menu = arr[0];
+            String option = null;   // 주소 없음
+
+            if(arr.length == 2) {
+                option = arr[1];
+            }
+
+
+            if(menu.equals("quit")) {
+                System.out.println("안녕히 가세요!");
+                break;
+            } else if (menu.equal("help")) {
+                System.out.println("[도움말]");
+                System.out.println("팀 등록 명령 : team/add");
+                System.out.println("팀 조회 명령 : team/list");
+                System.out.println("팀 상세조회 명령 : team/view 팀명");
+                System.out.println("회원 등록 명령 : member/add");
+                System.out.println("회원 조회 명령 : member/list");
+                System.out.println("회원 상세조회 명령 : member/view 아이디");
+                System.out.println("종료 : quit");
+                System.out.println();
+                continue;
+            } else if (menu.equals("team/add")) {
+                System.out.println("[팀 정보 입력]");
+                
+                // 먼저 팀 클래스를 만들고 생성해라!!
+                Team team = new Team();
+                
+
+                System.out.print("팀명? ");
+                team.name = keyScan.nextLine();
+
+                System.out.print("설명? ");
+                team.description = keyScan.nextLine();
+
+                System.out.print("최대인원? ");
+                team.name = keyScan.nextInt();
+                keyScan.nextLine();
+
+                System.out.print("시작일? ");
+                team.startDate = keyScan.nextLine();
+                
+                System.out.print("팀명? ");
+                team.endDate = keyScan.nextLine();
+
+                // 팀 정보가 담겨있는 객체의 주소를 배열에 보관한다.
+                teams[teamIndex++] = team;
+
+                System.out.println();
+                
+                continue;
+            } else if (menu.equals("team/list")) {
+                System.out.println("[팀 목록]");
+                for(int i = 0; i < teamIndex; i++) {
+                    System.out.printf("%s, %d, %s, %s\n", 
+                        teams[i].name,  teams[i].name, 
+                        teams[i].startDate, teams[i].endDate);                   
+                }
+                System.out.println();
+                countinue;
+            } else if (menu.equals("team/view")) {
+                System.out.println("[팀 정보 조회]");
+
+                if(option == null) {
+                    System.out.println("팀명을 입력하시기 바랍니다.");
+                    System.out.println();
+                    continue;
+                }
+
+                for (int i = 0; i < teamIndex; i++) {
+                    if (option.equals(teams[i].name.toLowerCase())) {
+                        System.out.printf("팀명 : %s\n", teams[i].name);
+                        System.out.printf("설명 : %s\n", teams[i].description);
+                        System.out.printf("최대인원 : %d\n", teams[i].maxQty);
+                        System.out.printf("기간 : %s ~ %s\n",
+                            teams[i].startDate, teams[i].endDate);
+                        break;
+                    }
+                }
+
+                System.out.println();
+            }
+
+            System.out.println(menu);
+        }
+        
+    }
+}
 
 
 */
