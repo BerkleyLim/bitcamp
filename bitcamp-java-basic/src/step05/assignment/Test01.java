3b06b294ff10ed3ebd0eb2d081aa28e7d40768fb
psk041e <tjrud64@gmail.com>
2018-03-13 14:36:25 +0900

    test

---
+++ bitcamp-java-basic/src/step05/assignment/Test01.java
@@ -0,0 +1,76 @@
+package step05.assignment;
+
+import java.util.Scanner;
+public class Test01 {
+    public static void main(String[] args) {
+        int num;   
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("밑변 길이? ");
+        num = sc.nextInt();
+
+        // 직사각형 출력!! - while 문
+        int i = 0;
+        while(i < num) {
+            int j = 0;
+            while(j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+
+        System.out.println("--------------------------------------");
+
+        // do ~ while문형
+        i = 0;
+        do {
+            int j = 0;
+            do {
+                System.out.print("*");
+                j++;
+            } while(j <= i);
+            System.out.println();
+            i++;
+        } while(i < num);
+    }
+}
+
+
+/*
+<강사님 코드>
+package step05.assignment;
+
+import java.util.Scanner;
+
+public class Test01 {
+    public static void main(String[] args) {
+        Scanner keyScan = new Scanner(System.in);
+        System.out.println("밑변의 길이? ");
+        int len = keyScan.nextInt();
+
+        int count = 1;
+        while(count <= len) {
+            int starCnt = 1;
+            while(StarCnt <= count) {
+                System.out.print("*");
+                StartCnt++;
+            }
+            System.out.println();
+            count++;
+        }
+    }
+}
+*/
+
+/*
+C:\Users\Bit\git\bitcamp\bitcamp-java-basic>java -cp bin step05.assignment.Test01
+밑변 길이? 5
+*
+**
+***
+****
+*****
+*/
