4ccd89d1fbd295405d051e41ce5754c6e1456018
berkleylim <berkleylim16@gmail.com>
2018-08-16 17:43:46 +0900

    2018.8.16 (목)

--- bitcamp-java-project/src/main/webapp/html7/js/common.js
+++ bitcamp-java-project/src/main/webapp/html7/js/common.js
@@ -1,2 +1,3 @@
-var serverRoot = "http://18.220.208.235:8888/bitcamp-java-project";
+var serverRoot = "http://www.berkleylim.xyz:8888/pms";
+	//"http://18.220.208.235:8888/bitcamp-java-project";
 	//"http://localhost:8888/bitcamp-java-project";
